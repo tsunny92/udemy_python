a8ab471d6f35a920908e17cf073fcf8b8ffed80f
Sunny.T <sunny.t@centos7Office.com>
2018-11-19 21:47:46 +0530

    Final device42 script

--- device42.py
+++ device42.py
@@ -4,33 +4,58 @@ import json
 import urllib3
 import getpass
 
-device42url='https://dcmgr.hou1.hostgator.com/api/1.0/devices/all/?name='
+
 urlpass='https://dcmgr.hou1.hostgator.com/api/1.0/passwords/?device='
+device42url='https://dcmgr.hou1.hostgator.com/api/1.0/devices/name/'
 
 hostname=raw_input("Enter the hostname : ")
 username=raw_input("Enter username : ")
 password=getpass.getpass("Enter the password : ")
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 response = requests.get(device42url+hostname, verify=False, auth=(username, password))
 passresponse = requests.get(urlpass+hostname+"&plain_text=yes", verify=False, auth=(username, password))
 jsondata= json.loads(response.text)
 passdata= json.loads(passresponse.text)
-list1=jsondata['Devices'][0]['ip_addresses']
+
 
 print("------------------------- Details for "+hostname+" -------------------------")
-print("Manufacturer : "+jsondata['Devices'][0]['manufacturer'])
-print('Service Level : '+jsondata['Devices'][0]['service_level'])
-print('Type :'+jsondata['Devices'][0]['type'])
-print("Hardware model :"+jsondata['Devices'][0]['hw_model'])
-print("Password for Management : "+passdata['Passwords'][0]['password'])
-print("Password for IPMI : "+passdata['Passwords'][1]['password']) 
 
+if str(jsondata['hw_model']) == 'None': 
+	pass
+else:
+	print('Hardware model : '+jsondata['hw_model'])
 
-for i in range(len(list1)):
-	if jsondata['Devices'][0]['ip_addresses'][i]['label'].strip() == 'IPMI':
-		print(jsondata['Devices'][0]['ip_addresses'][i]['label'].strip() +" Address is "+ jsondata['Devices'][0]['ip_addresses'][i]['ip'])
-	elif jsondata['Devices'][0]['ip_addresses'][i]['label'].strip() == 'Management':
-		print(jsondata['Devices'][0]['ip_addresses'][i]['label'].strip() +" Address is "+ jsondata['Devices'][0]['ip_addresses'][i]['ip'])
-	else:
+if str(jsondata['manufacturer']) =='None': 
+	pass
+else:
+	print('Manufacturer : '+jsondata['manufacturer'])
+
+if str(jsondata['os']) == 'None': 
+	pass
+else:
+	print("Operating system : "+jsondata['os'])
+
+print('Service Level : '+jsondata['service_level'])
+
+if jsondata['type'] == 'virtual':
+	print('Type : '+jsondata['type'])
+	print('Host machine '+jsondata['virtual_host_name'])
+else:
+	print('Type : '+jsondata['type'])
+
+length=len(jsondata['ip_addresses'])
+
+for i in range(length):
+	if jsondata['ip_addresses'][i]['label'] == '' or str(jsondata['ip_addresses'][i]['label']) == 'None':
 		pass
+	else:
+		print(jsondata['ip_addresses'][i]['label'].strip()+' address is : '+jsondata['ip_addresses'][i]['ip'])
+
+
+for i in range(len(passdata['Passwords'])):
+	if passdata['Passwords'][i]['label'] == '':
+		pass
+	else:
+		print(passdata['Passwords'][i]['label'].strip()+' password is '+passdata['Passwords'][i]['password'])
