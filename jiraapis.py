7d57eb3211caea265a802942aecfc14182eabbd0
tsunny92 <35867571+tsunny92@users.noreply.github.com>
2019-03-05 03:03:02 +0530

    Update jiraapis.py

--- jiraapis.py
+++ jiraapis.py
@@ -4,11 +4,13 @@ import json
 import urllib3
 import getpass
 import os
+from os import path
 
 jiraurl='https://jira.endurance.com/rest/api/2/search?jql=description~%22List%20of%20Affected%22%20and%20project=%22Enterprise%20-%20Monitoring%20Center%20Server%20Issues%22%20and%20status=open&fields=id,key,description'
 
 username=raw_input("Enter username : ")
 password=getpass.getpass("Enter the password : ")
+
 
 # Disalbe or ignore ssl verification warnings message
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -20,23 +22,26 @@ jsondata= json.loads(response.text)
 count = len(jsondata['issues'])
 
 def datacollect():
-    for i in range(count):
-        data =  str(jsondata['issues'][i]['key']) + '\n' + str(jsondata['issues'][i]['fields']['description'])
-        with open('/root/datafile.txt' , 'w+') as f:
-            f.write(data)
+	if count > 0:
+    		for i in range(count):
+        		data =  str(jsondata['issues'][i]['key']) + '\n' + str(jsondata['issues'][i]['fields']['description'])
+			with open('/home/sunny.t/datafile.txt' , 'w+') as f:
+        			f.write(data)
+	else:
+		print("No issues found")
 
-
-
-def listoftlds():
-    file = open('/root/datafile.txt').read().splitlines()
-    for index, line in enumerate(file):
-        if 'EMCSI' in line or 'Start time' in line or 'End time' in line or 'TLDs' in line:
-            print(line)
-        elif "---" in line :
-            for data in file[index+1:index+1000]:
-                if not '---' in data:
-                    print(data)
-    os.remove('/root/datafile.txt')
-
+def listoftlds():	
+	if path.exists('/home/sunny.t/datafile.txt'):
+    		file = open('/home/sunny.t/datafile.txt').read().splitlines()
+    		for index, line in enumerate(file):
+        		if 'EMCSI' in line or 'Start time' in line or 'End time' in line:
+            			print(line)
+        		elif "TLDs" in line :
+            			for data in file[index+1:index+1000]:
+                			if not '---' in data:
+                    				print(data)
+    		os.remove('/home/sunny.t/datafile.txt')
+	else:
+		print("No file found")
 datacollect()
 listoftlds()
